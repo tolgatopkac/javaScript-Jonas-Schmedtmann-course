4e5a862c48245dbd1b8e0b682406b5009409962e
Tolga Topkaç <tolgatopkac09@gmail.com>
2021-07-01 13:21:16 +0300

    update index.html, script.js and create myNotes.js for my notes

---
+++ Fundamentals-Part-1/myNotes.js
@@ -0,0 +1,11 @@
+//JavaScript is a high-level object-oriented, multi-paradigm programming language.
+
+// The role of JavaScript in Web Development
+// -- HTML(nouns) > content
+// -- CSS(adjectives) > Presentation
+// -- JavaScript(Verbs) > Programming Language building web applications
+
+// -- Dynamic effects and web applications in the browser  (React, Angular, Vue)  -- Front-end apps
+// -- Web Applications on web servers (node.js) -- Back-end apps
+// -- Native mobile applications (react native, ionic)
+// -- Native desktop applications (electron)
